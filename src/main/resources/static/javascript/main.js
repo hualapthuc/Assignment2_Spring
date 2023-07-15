90862c8f767040d1a31cc7cfaf142cd9d880b675
Tuyen <vuavntuyen775@gamil.com>
2023-07-15 17:40:38 +0700

    sua loi tiep tiep tiep

--- src/main/resources/static/javascript/main.js
+++ src/main/resources/static/javascript/main.js
@@ -1,10 +1,3 @@
 function  deleteContennt() {
-    var result = confirm("Do you want DELETE?");
-
-    if(result == true){
-        alert("Yes");
-    }
-    else{
-        alert("cancel");
-    }
+  confirm("Do you want DELETE?");
 }
